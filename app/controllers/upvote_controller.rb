f5124bdfcdec5a932311d9fbbbc42da1d4aeb5d5
Diksha2008 <dikshamanchanda20@gmail.com>
2017-07-20 23:59:40 +0530

    Add upvote functionality

--- app/controllers/upvote_controller.rb
+++ app/controllers/upvote_controller.rb
@@ -1,2 +1,14 @@
 class UpvoteController < ApplicationController
+
+	# POST /upvote
+	def newUpvote
+		answer = Answer.find(params[:ans_id])
+		upvote = Upvote.where(users: current_user, answer: answer).first
+		if upvote
+			upvote.destroy!
+		else
+			Upvote.create(users_id: current_user.id, answer_id: answer.id)
+		end	
+		return redirect_to '/'
+	end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,15 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+
+  def upvoted_by user_id
+  	Upvote.where(users_id: user_id, answer_id: id).first
+  end
+  def is_upvoted user_id
+  	if upvoted_by user_id
+  		"Upvoted"
+  	else
+  		"Upvote"
+  	end
+  end
 end
